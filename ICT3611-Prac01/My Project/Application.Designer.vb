6cc094448aa7885f3beddf38e635ddcf2a32d844
Yobyn Roetz <48220337@mylife.unisa.ac.za>
2014-07-20 08:11:38 +0200

    Prac01 Complete
    Done with Prac01

--- ICT3611-Prac01/My Project/Application.Designer.vb
+++ ICT3611-Prac01/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ICT3611_Prac01.Form1
+            Me.MainForm = Global.ICT3611_Prac01.Prac01
         End Sub
     End Class
 End Namespace
